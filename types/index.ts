6a9da1e14244e8984a2ebe2f07d7c3ea7fdb401c
RrrS2022 <sun.yuhu@northeastern.edu>
2025-04-27 17:36:18 -0400

    clean up unused interface

--- types/index.ts
+++ types/index.ts
@@ -16,11 +16,3 @@ export enum TaskStatus {
     PENDING = "pending",
     COMPLETED = "completed"
 }
-
-/**
- * interface for forming the input data
- */
-export interface TaskForm {
-    title: string;
-    description: string;
-}
